5bb5b8e2df0b839527930810252d4a88213ae9db
03k09i <phankiet0309@gmail.com>
2024-06-25 22:01:48 +0700

    Update UI Admin

--- admin_client/src/pages/_app.js
+++ admin_client/src/pages/_app.js
@@ -63,9 +63,60 @@ function MyApp({ Component, pageProps }) {
 				algorithm: mode === 'dark' ? theme.darkAlgorithm : theme.defaultAlgorithm,
 				components: {
 					Menu:{
-						iconSize: 30,
-						itemColor: 'yellow',
-						itemSelectedColor: 'black'
+						fontSize: 16,
+						itemColor: '#00b4d8',
+						itemSelectedColor: '#023e8a',
+						itemActiveBg: '#00b4d8',
+						itemHoverColor: '#023e8a',	
+						itemHeight: 36,					
+					},
+					Input: {
+						colorTextPlaceholder: '#0077B6',
+						colorBorder: '#0077B6',
+						borderRadius: 10,
+					},
+					Tooltip: {
+						colorBgSpotlight: '#03045E',
+					},
+					Divider: {
+						colorSplit: 'rgb(3, 4, 94, 0.5)'
+					},
+					Typography: {
+						colorText: '#023E8A'
+					},
+					Modal: {
+						titleColor: '#023E8A'
+					},
+					Button: {
+						colorPrimary: '#023E8A',
+						colorPrimaryHover: '#0077B6',
+						defaultColor: '#0077B6',
+						colorBorder: '#0077B6',
+					},
+					Card: {
+						actionsLiMargin: '7px 0',
+						colorTextHeading: '#0077B6',
+						headerFontSize: 14,
+						headerHeight: 45,
+					},
+					Popover: {
+						boxShadow: 'rgba(0, 119, 182, 0.25) 0px 0px 5px',
+					},
+					Form: {
+						labelColor: '#023E8A',
+						itemMarginBottom: 14,
+					},
+					Select: {
+						colorBorder: '#0077B6',
+						colorText: '#023E8A',
+						boxShadow: 'rgba(0, 119, 182, 0.25) 0px 0px 5px !important',
+						borderRadius: '8px !important'
+					},
+					Steps: {
+						colorSplit: '#90E0EF',
+					},
+					List:{
+						titleMarginBottom: 0,
 					}
 				}
 			}}
